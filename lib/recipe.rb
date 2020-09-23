93e9dc1cc580b63336048f6193e16c551be65d7f
Sean Steel <seanrsteel@gmail.com>
2020-09-23 10:49:13 -0600

    Finish ingredients by name method in recipe, test passes

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -13,10 +13,16 @@ class Recipe
     @ingredients_required.keys
   end
 
+  def ingredients_by_name
+    ingredients.map do |ingredient|
+      ingredient.name
+    end 
+  end
+
   def total_calories
     @ingredients_required.sum do |ingredient, quantity|
       ingredient.calories * quantity
-    end 
+    end
   end
 end
 
