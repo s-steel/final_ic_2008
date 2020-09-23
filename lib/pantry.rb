e3a4e550801e71728b147a0bae81a93200345822
Sean Steel <seanrsteel@gmail.com>
2020-09-23 09:15:23 -0600

    Create test for stock check method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,6 +1,6 @@
 class Pantry
   attr_reader :stock
   def initialize
-    @stock = {}
-  end 
+    @stock = Hash.new(0)
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,11 +7,17 @@ class PantryTest < Minitest::Test
 
   def test_it_has_attributes
     pantry = Pantry.new
-    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     expected = {}
 
     assert_instance_of Pantry, pantry
     assert_equal expected, pantry.stock
   end
+
+  def test_check_stock
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end 
 end
