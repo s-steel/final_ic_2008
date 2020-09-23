a8cc3038c957eb259c14448d53ee1841f7934f51
Sean Steel <seanrsteel@gmail.com>
2020-09-23 09:58:29 -0600

    Add total calories method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -12,4 +12,13 @@ class Recipe
   def ingredients
     @ingredients_required.keys
   end
+
+  def total_calories
+    @ingredients_required.sum do |ingredient, quantity|
+      ingredient.calories * quantity
+    end 
+  end
 end
+
+
+# require 'pry'; binding.pry
